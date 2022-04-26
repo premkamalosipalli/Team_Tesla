71e830974d01a72e7b035764cbbf1bfc39811a76
Prem Kamal Osipalli <kamalosipalli@gmail.com>
2022-04-25 23:44:13 -0500

    deleted unwanted data

--- MovieBooking/src/main/java/com/movie/booking/controller/JWebUnitTest.java
+++ MovieBooking/src/main/java/com/movie/booking/controller/JWebUnitTest.java
@@ -2,11 +2,10 @@ package com.movie.booking.controller;
 
 import static org.junit.Assert.assertEquals;
 
-import java.io.IOException;
+
 import java.io.PrintWriter;
 
 import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -18,17 +17,16 @@ import org.mockito.Mockito;
 
 import com.movie.booking.model.LoginDb;
 
-import junit.framework.TestCase;
-
 
 public class JWebUnitTest{
 
 
-	/* private LoginServlet loginUser;
+	private LoginServlet loginUser;
     private HttpServletRequest mockedRequest;
     private HttpServletResponse mockedResponse;
     private ServletContext mockedServletContext;
     private HttpSession mockedSession;
+    private LoginDb loginDb;
     
     private PrintWriter mockedOut;
     
@@ -46,25 +44,25 @@ public class JWebUnitTest{
     
     @AfterEach
     void tearDown() throws Exception {
-    }*/
+    }
     
     
     @Test
     final void testDoPostSuccessful() throws Throwable{
         
-    	// loginDb = new LoginDb();;
-    	/*Mockito.doReturn(mockedServletContext).when(mockedRequest).getServletContext();
+    	loginDb = new LoginDb();
+    	Mockito.doReturn(mockedServletContext).when(mockedRequest).getServletContext();
         Mockito.when(mockedRequest.getParameter("email")).thenReturn("email");
         Mockito.when(mockedRequest.getParameter("password")).thenReturn("password");
         Mockito.when(mockedRequest.getSession()).thenReturn(mockedSession);
         Mockito.when(loginDb.userLogin("email", "password"));
         Mockito.when(mockedResponse.getWriter()).thenReturn(mockedOut);
 
-        loginUser.doPost(mockedRequest, mockedResponse);*/
+        loginUser.doPost(mockedRequest, mockedResponse);
     	assertEquals("prem@gmail.com", "prem@gmail.com");
-        //assertEquals("prem@gmail.com", loginDb.userLogin("email", "password"));
-        //Mockito.verify(mockedOut).println("1");
-        //Mockito.verify(mockedSession).setAttribute(Mockito.eq("user"), loginDb);
+        assertEquals("prem@gmail.com", loginDb.userLogin("email", "password"));
+        Mockito.verify(mockedOut).println("1");
+        Mockito.verify(mockedSession).setAttribute(Mockito.eq("user"), loginDb);
     }
 
 }
